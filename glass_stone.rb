2ea6eabf1eacbd834fddd777f502cdefe08289f4
Danie Roux <danie@finworks.biz>
2009-08-14 16:37:10 +0200

    Turn off Monticello autoCommit and autoMigrate
    When running a snippet in Topaz, you need to control when the transaction commits. This is especially important if you have your own migrations to get done.

--- glass_stone.rb
+++ glass_stone.rb
@@ -2,6 +2,15 @@ require File.join(File.dirname(__FILE__), 'stone')
 
 class GlassStone < Stone
 
+  def run_topaz_commands(*commands)
+    begin
+      topaz_commands(["run", "MCPlatformSupport", "%"])
+      super(commands.unshift("MCPlatformSupport autoCommit: false; autoMigrate: false"))
+    rescue TopazError
+      super
+    end
+  end
+  
   def seaside_bin_directory
     "#{gemstone_installation_directory}/seaside/bin"
   end
