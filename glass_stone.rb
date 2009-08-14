17ef690fc7fd1e8ad8be1834ebda0dd4d1fb0afd
Danie Roux <danie@finworks.biz>
2009-08-14 22:36:18 +0800

    GLASS now bootstraps differently, remove old installMonticello
    Signed-off-by: MagLev Engineering Team <is@gemstone.com>

--- glass_stone.rb
+++ glass_stone.rb
@@ -1,13 +1,6 @@
 require File.join(File.dirname(__FILE__), 'stone')
 
 class GlassStone < Stone
-  def initialize_new_stone
-    super
-    start
-    run_topaz_command("UserGlobals at: #BootStrapSymbolDictionaryName put: #UserGlobals. System commitTransaction")
-    topaz_commands(["input #{gemstone_installation_directory}/seaside/topaz/installMonticello.topaz", "commit"])
-    run_topaz_command("UserGlobals removeKey: #BootStrapSymbolDictionaryName. System commitTransaction")
-  end
 
   def seaside_bin_directory
     "#{gemstone_installation_directory}/seaside/bin"
