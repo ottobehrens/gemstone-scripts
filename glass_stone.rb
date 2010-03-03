d90d720f6e38312066e6d0cdd0e9b169cba6a501
Willy Wonka <wonka@samuel.finworks.biz>
2010-03-03 11:16:35 +0200

    Moved lighty reverse proxying & daemontools management into glass_stone

--- glass_stone.rb
+++ glass_stone.rb
@@ -14,4 +14,156 @@ class GlassStone < Stone
   def seaside_bin_directory
     "#{gemstone_installation_directory}/seaside/bin"
   end
+
+  def GlassStone.clear_status
+    sh "svc -o /service/clear"
+  end
+
+  def create_daemontools_structure
+    services_names.each do |service_name|
+      service_directory = "/service/#{service_name}"
+      fail "Service directory #{service_directory} exists, please remove it manually, ensuring all services are stopped" if File.exists? service_directory
+      mkdir_p "#{service_directory}/log"
+      service_skeleton = "#{File.expand_path(File.dirname(__FILE__))}/service_skeleton"
+      sh "cd #{service_skeleton}; find -path ./gemstone-scripts/.git -prune -o -print | cpio -p --dereference #{service_directory}"
+      touch "#{service_directory}/down"
+    end
+  end
+
+  def remove_daemontools_structure
+    services_names.each do |service_name|
+      service_directory = "/service/#{service_name}"
+      rm_rf service_directory if File.exists? service_directory
+    end
+  end
+
+  def start_hypers
+    GlassStone.clear_status
+    services_names.each { |service_name| sh "svc -u /service/#{service_name}" }
+  end
+
+  def start_hyper(port)
+    if pid=fork
+      system("nohup glass-hyper #{port} #{name} &")
+      Process.detach(pid)
+    end
+  end
+
+  def start_hyper_fg(port)
+    raise 'Environment variable LANG not set, you are probably running this from a restricted shell - bailing out' if not ENV['LANG'] 
+    system("glass-hyper #{port} '#{name}'")
+  end
+
+  def start_system
+    super
+    start_hypers
+  end
+
+  def stop_system
+    stop_hypers
+    super
+  end
+
+  def stop
+    fail "Daemontools still attempting to start hypers, consider stop_hypers." if any_hyper_supposed_to_be_running?
+    fail "Some hypers still running. Consider stopping them first." if any_hyper_running?
+    super
+  end
+
+  def stop_hypers
+    services_names.each { |service_name| sh "svc -d /service/#{service_name}" }
+    sleep 1
+    fuser_hyper_ports("-k")
+  end
+
+  def status_hypers
+    hyper_ports.each do | port |
+      status_hyper_port(port)
+    end
+  end
+
+  def any_hyper_running?
+    !(hyper_ports.detect {| port | status_hyper_port(port)}).nil?
+  end
+
+  def any_hyper_supposed_to_be_running?
+    services_names.detect do |service_name|
+      !(`svstat /service/#{service_name}` =~ /service\/#{service_name}: up/).nil?
+    end
+  end
+
+  def hyper_ports
+    hyper_ports_lighty
+  end
+
+  def lighty_config
+    Dir["/etc/lighttpd/conf-available/99-*.conf"].inject('') do | result, config_file_name |
+      result + File.open(config_file_name).read
+    end
+  end
+
+  def status
+    super
+    status_hypers
+  end
+
+  def lighty_config_template(ports)
+    return <<-TEMPLATE
+$HTTP["host"] == "#{name}" {
+  proxy.server  = ( "" => ( 
+#{generate_ports(ports)}
+                          ) )
+}
+    TEMPLATE
+  end
+
+  def create_lighty_config(ports)
+    File.open("/etc/lighttpd/conf-available/99-#{name}.conf", "w+") do | file |
+      file.print(lighty_config_template(ports))
+    end
+  end
+
+  def remove_lighty_config
+    config = "/etc/lighttpd/conf-available/99-#{name}.conf"
+    File.delete(config) if File.exists? config
+  end
+
+  def generate_ports(ports)
+    ports.collect { | port | "\t\t\t( \"host\" => \"127.0.0.1\", \"port\" => #{port} ),\n" }
+  end
+
+  def hyper_ports_lighty
+    lighty_config.scan(/HTTP\["host"\][^}]*\}/).each do | section | 
+      if section =~ /#{name}/
+        return section.scan(/"port" => (\d{4})/).flatten
+      end
+    end 
+    fail "Could not find ports for #{name} in #{lighty_config}"
+  end
+
+  def services_names
+    hyper_ports_lighty.collect { | port | "#{name}-#{port}" }
+  end
+
+  def status_hyper_port(port)
+    sh "svstat /service/#{name}-#{port}"
+    fuser_hyper_port(port)
+  end
+
+  def fuser_hyper_ports(extra_flags = "")
+    hyper_ports.each do |port|
+      fuser_hyper_port(port, extra_flags)
+    end
+  end
+
+  def fuser_hyper_port(port, extra_flags = "")
+    sh "echo #{name}; fuser #{extra_flags} -n tcp #{port}" do |ok, status|
+      if !ok
+        puts "Could not find a hyper on port #{port}"
+      end
+      return ok
+    end
+    return false
+  end
 end
+
---
+++ tests/glass_stone_test_case.rb
@@ -0,0 +1,108 @@
+#!/usr/bin/ruby
+
+require File.join(File.dirname(__FILE__), '..', 'glass_stone')
+require 'common_test_case'
+
+class Date
+  def to_Ymd_string
+    strftime("%Y-%m-%d")
+  end
+end
+
+class GlassStoneTestCase < BaseTestCase
+  def test_find_port_numbers_from_lighty
+    stone, mock_stone = mock_stone_lighty_config(TEST_STONE_NAME)
+    assert_equal(['8000', '1234'],stone.hyper_ports_lighty)
+    stone, mock_stone= mock_stone_lighty_config("agdev.lan")
+    assert_equal(['9000', '9001'],stone.hyper_ports_lighty)
+  end
+
+  def test_hyper_service_needs_lang_environment_variable
+    ENV['LANG'] = nil
+    assert_raises(RuntimeError) { GlassStone.new(nil).start_hyper_fg(1234) }
+  end
+
+  def test_services_names
+    stone, mock_stone = mock_stone_lighty_config(TEST_STONE_NAME)
+    assert_equal(["#{TEST_STONE_NAME}-8000", "#{TEST_STONE_NAME}-1234"], stone.services_names)
+  end
+
+  def test_hypers_start
+    stone, mock_stone = mock_stone_lighty_config(TEST_STONE_NAME)
+    mock_stone.should_receive(:sh).with("svc -u /service/#{TEST_STONE_NAME}-8000").once
+    mock_stone.should_receive(:sh).with("svc -u /service/#{TEST_STONE_NAME}-1234").once
+    stone.start_hypers
+  end
+
+  def test_hypers_start_for_second_stone
+    stone, mock_stone = mock_stone_lighty_config('stone2.finworks.biz')
+    mock_stone.should_receive(:sh).with("svc -u /service/stone2.finworks.biz-5000").once
+    mock_stone.should_receive(:sh).with("svc -u /service/stone2.finworks.biz-9876").once
+    stone.start_hypers
+  end
+
+  def mock_stone_lighty_config(stone_name)
+    stone = GlassStone.new(stone_name)
+    partial_mock_stone = flexmock(stone)
+    partial_mock_stone.should_receive(:lighty_config).and_return(mock_lighty_config)
+    return stone, partial_mock_stone
+  end
+
+  def mock_lighty_config
+    <<-MOCK
+    $SERVER["socket"] == "0.0.0.0:443" {
+                      ssl.engine                  = "enable"
+                      ssl.pemfile                 = "/etc/lighttpd/server.pem"
+
+      $HTTP["host"] == "#{TEST_STONE_NAME}" {
+          proxy.server  = ( "" => (( "host" => "127.0.0.1", "port" => 8000 ),
+                                   ( "host" => "127.0.0.1", "port" => 1234 ), ))
+      }
+      $HTTP["host"] =~ "agdev.lan" {
+          proxy.server  = ( "" => (( "host" => "127.0.0.1", "port" => 9000 ),
+                                   ( "host" => "127.0.0.1", "port" => 9001 ), ))
+      }
+    }
+
+    $SERVER["socket"] == "1.2.3.4:443" {
+                      ssl.engine                  = "enable"
+                      ssl.pemfile                 = "/etc/lighttpd/server.pem"
+
+      $HTTP["host"] == "stone2.finworks.biz" {
+          proxy.server  = ( "" => (( "host" => "127.0.0.1", "port" => 5000 ),
+                                   ( "host" => "127.0.0.1", "port" => 9876 ), ))
+      }
+    }
+    MOCK
+  end
+end
+
+class GlassStoneIntegrationTestCase < BaseTestCase
+
+  def setup
+    super
+    @stone = GlassStone.create(TEST_STONE_NAME)
+  end
+
+  def test_hypers_status
+    @stone.create_lighty_config([9999])
+    @stone.create_daemontools_structure
+    deny(@stone.any_hyper_supposed_to_be_running?) 
+    deny(@stone.any_hyper_running?) 
+    @stone.start
+    deny(@stone.any_hyper_supposed_to_be_running?) 
+    deny(@stone.any_hyper_running?) 
+    supervise_ok = "/service/#{@stone.services_names.first}/supervise/ok"
+    while (not File.exists? supervise_ok) do
+      sleep 1
+      puts "Waiting for #{supervise_ok}"
+    end
+    @stone.start_hypers
+    sleep 3
+    assert(@stone.any_hyper_supposed_to_be_running?) 
+    assert(@stone.any_hyper_running?) 
+    @stone.stop_hypers
+    @stone.remove_daemontools_structure
+    @stone.remove_lighty_config
+  end
+end
