f4729cf980abc9f7eb04269bd64f9792002ede91
Pieter Jacobs <pieter@finworks.biz>
2013-04-30 13:58:29 +0200

    Passing arguments as a hash for all methods called from rake

--- stone.rb
+++ stone.rb
@@ -152,11 +152,11 @@ class Stone
   end
 
   def restore_latest_full_backup
-    restore
+    restore({})
   end
 
   def restore_full_backup(stone_name, for_date=Date.today)
-    restore(backup_filename(stone_name, for_date))
+    restore({:file_name => backup_filename(stone_name, for_date)})
   end
 
   def restore_full_backup_from_named_file(file_name=backup_filename_for_today)
